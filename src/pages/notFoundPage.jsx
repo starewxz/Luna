e0137849923cbc655e81b3031b2a21cbd6ebec3a
Stanislav Revasevych <revasevicstanislav@gmail.com>
2025-07-01 18:38:12 +0300

    Upgrade styling, still without AI

--- src/pages/notFoundPage.jsx
+++ src/pages/notFoundPage.jsx
@@ -1,11 +1,17 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import {useNavigate} from "react-router-dom";
 
 
 const NotFoundPage = () => {
+    const navigate = useNavigate();
   return (
       <div className={""}>
           <Header />
+          <main>
+              <p>404 NOT FOUND</p>
+              <button onClick={() => navigate("/")} >Return to the main page</button>
+          </main>
           <Footer />
       </div>
   )
